dc4ce7afeb0f8d1fbdf2ce558683948cb786ea56
Matúš Petrofčík (Fedora 22) <petrofcikmatus@gmail.com>
2015-12-08 15:04:33 +0200

    textfields

--- src/com/awt02/Homework02.java
+++ src/com/awt02/Homework02.java
@@ -5,6 +5,7 @@ import java.awt.event.*;
 
 public class Homework02 extends Frame implements WindowListener, MouseMotionListener, MouseListener {
 
+    TextField tfX=null,tfY=null;
     private int radius = 10; // Xpx radius of Oval
 
     private int size = 500;
@@ -13,14 +14,25 @@ public class Homework02 extends Frame implements WindowListener, MouseMotionList
         setTitle("Java AWT Homework 2 - Matúš Petrofčík");
 
         setSize(size, size);
-        setLayout(new FlowLayout());
+        setLayout(new FlowLayout(FlowLayout.CENTER));
         setResizable(false);
 
         this.addMouseMotionListener(this);
         this.addMouseListener(this);
+        add(new Label("X="));
+        tfX=new TextField(5);add(tfX);
+
+        add(new Label("Y="));
+        tfY=new TextField(5);add(tfY);
 
         addWindowListener(this);
         setVisible(true);
+    }
+
+    public void paint(Graphics g){
+        g.setColor(Color.black);
+        g.drawLine(0,size/2,size, size/2);
+        g.drawLine(size/2,0, size/2, size);
     }
 
     private void painDot(int x, int y){
@@ -55,6 +67,7 @@ public class Homework02 extends Frame implements WindowListener, MouseMotionList
 
     @Override
     public void windowClosed(WindowEvent windowEvent) {
+        add(new Label("X="));
 
     }
 
@@ -87,7 +100,9 @@ public class Homework02 extends Frame implements WindowListener, MouseMotionList
     public void mouseMoved(MouseEvent mouseEvent) {
         int mX = (int) mouseEvent.getPoint().getX();
         int mY = (int) mouseEvent.getPoint().getY();
-
+        tfX.setText(String.format("%d", mX));
+        tfY.setText(String.format("%d", mY));
+//        repaint();
         setTitle(String.format("Súradnice myši sú %d a %d", mX, mY));
     }
 
